f799f9096536753fcc9287db49cf760b414b5e10
Mark Pahor <mark@Marks-MBP.socal.rr.com>
2019-07-26 22:21:38 -0700

    orm calls to select all, add one, and update a burger done and updated in model

--- config/orm.js
+++ config/orm.js
@@ -3,35 +3,31 @@ var connection = require("../config/connection.js");
 
 
 var orm = {
-    // Select All to display all burgers added to the db
-    selectAll: function(tableInput, colToSearch, valOfCol) {
-      var queryString = "SELECT * FROM ?? WHERE ?? = ?";
-      connection.query(queryString, [tableInput, colToSearch, valOfCol], function(err, result) {
+    // Select All to display all burgers added to the db - selecting all burgers in db to display to html
+    selectAll: function(tableInput) {
+      var queryString = "SELECT * FROM ??";
+      connection.query(queryString, [tableInput], function(err, result) {
         if (err) throw err;
         console.log(result);
       });
     },
 
-    //insertOne to allow the user to add a new burger to the list
-    insertOne: function(whatToSelect, table, orderCol) {
-      var queryString = "SELECT ?? FROM ?? ORDER BY ?? DESC";
+    //insertOne to allow the user to add a new burger to the list - INSERT INTO tableInput(??) (burger_name(??) VALUES ?
+    insertOne: function(tableInput, colName, burgerName) {
+      var queryString = "INSERT INTO ?? (??) VALUES (?)";
       console.log(queryString);
-      //whatToselect, table, orderCol are names you're making up for the parameters that you are then defining and passing through server.js to run the sql queries
-      connection.query(queryString, [whatToSelect, table, orderCol], function(err, result) {
+      connection.query(queryString, [tableInput, colName, burgerName], function(err, result) {
         if (err) throw err;
         console.log(result);
       });
     },
 
     //updateOne to change a burger from not eaten to devoured - changing boolean devoured from false to true 
-    updateOne: function(tableOneCol, tableTwoForeignKey, tableOne, tableTwo) {
-      var queryString =
-        "SELECT ??, COUNT(??) AS count FROM ?? LEFT JOIN ?? ON ??.??= ??.id GROUP BY ?? ORDER BY count DESC LIMIT 1";
+    //UPDATE tableInput SET colName = true WHERE id = id
+    updateOne: function(tableInput, colName, id) {
+      var queryString = "UPDATE ?? SET ?? = true WHERE id = ?"
   
-      connection.query(
-        queryString,
-        [tableOneCol, tableOneCol, tableOne, tableTwo, tableTwo, tableTwoForeignKey, tableOne, tableOneCol],
-        function(err, result) {
+      connection.query(queryString,[tableInput, colName, id],function(err, result) {
           if (err) throw err;
           console.log(result);
         }
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,18 +1,22 @@
 //Require express
 var express = require("express");
 
-var router = express.Router();
+var app = express();
+
+var path = require("path");
+
 
 
 // Import burger model (burger.js) in order to use db functions defined in orm calls 
-var model = require("../controllers/burger.js");
+var model = require("../models/burger.js");
 
 // Create all routes and set up logic within those routes where required.(api routes)
 
-// Creating Route for index page (html routes)
-router.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "./views/index.handlebars"));
+// Creating Route for index page (html routes) - using placeholder html file until I set up handlebars
+
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 
-// Export routes for server.js to use.
-module.exports = router;
+
+module.exports = app;
